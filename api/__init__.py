81d9331017b80aefde0d5005061f57b3f6e265e5
Raj <raj.725@outlook.com>
2019-09-17 02:55:52 +0530

    first draft

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from api.config import STRIPE_SECRET_KEY
+from api.config import STRIPE_SECRET_KEY, STRIPE_PUB_KEY
 
 import stripe
 
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 stripe.api_key = STRIPE_SECRET_KEY
 
 
--- api/routes.py
+++ api/routes.py
@@ -1,12 +1,56 @@
 from flask import jsonify, make_response
 from flask import render_template, url_for, flash, redirect, request
-from api import app, stripe
+from api import app, stripe, STRIPE_PUB_KEY
 from api.models import Payment
+
+
+def accept_payment(payment_id):
+    payment = Payment.query.get_or_404(payment_id)
+    try:
+        customer = stripe.Customer.create(email=payment.email, source=payment.token)
+        charge = stripe.Charge.create(
+            customer=customer.id,
+            amount=int(payment.amount * 100),
+            currency='usd',
+            description='Widget Order'
+        )
+        payment.status = 'accepted'
+    except stripe.error.CardError as e:
+        # Since it's a decline, stripe.error.CardError will be caught
+        body = e.json_body
+        err = body.get('error', {})
+        print("Status is: %s" % e.http_status)
+        print("Type is: %s" % err.get('type'))
+        print("Code is: %s" % err.get('code'))
+        print("Param is: %s" % err.get('param'))
+        print("Message is: %s" % err.get('message'))
+        payment.status = 'failed'
+    except stripe.error.RateLimitError as e:
+        # Too many requests made to the API too quickly
+        print("Too many requests made to the API too quickly")
+        payment.status = 'failed'
+    except stripe.error.InvalidRequestError as e:
+        print("Invalid parameters were supplied to Stripe's API")
+        payment.status = 'failed'
+    except stripe.error.AuthenticationError as e:
+        print("Authentication with Stripe's API failed (maybe you changed API keys recently)")
+        payment.status = 'failed'
+    except stripe.error.APIConnectionError as e:
+        print("Network communication with Stripe failed")
+        payment.status = 'failed'
+    except stripe.error.StripeError as e:
+        print("Display a very generic error to the user, and maybe send yourself an email")
+        payment.status = 'failed'
+    except Exception as e:
+        print("Something else happened, completely unrelated to Stripe")
+        payment.status = 'failed'
+    payment.save()
 
 
 @app.route("/")
 def home():
-    return render_template('order-form.html')
+    amount = 100.0
+    return render_template('order-form.html', pub_key=STRIPE_PUB_KEY, amount=amount)
 
 
 @app.route('/order', methods=['POST'])
@@ -14,19 +58,38 @@ def place_order():
     name = request.form['name']
     email = request.form['email']
     instructions = request.form['instructions']
-    payment_info = request.form['paymentInfo']
-    payment = Payment(name=name, email=email, instructions=instructions)
+    stripeToken = request.form['stripeToken']
+    amount = request.form['amount']
+    payment = Payment(name=name, email=email, instructions=instructions, amount=amount, token=stripeToken)
     payment.save()
-    return jsonify({'datails': payment.name})
+    return render_template('thank_you.html', title="Thank You!")
 
 
 @app.route('/payments', methods=['GET'])
 def get_active_payments():
-    active_payments = Payment.query.all()
-    return jsonify({'active_payments': active_payments})
+    active_payments = Payment.query.filter_by(status='active').all()
+    return render_template('payments.html', payments=active_payments)
 
 
-@app.route('/payments/<id>', methods=['POST'])
-def process_payment():
-    active_payments = Payment.query.all()
-    return jsonify({'active_payments': active_payments})
+@app.route('/cancel/<payment_id>')
+def cancel_payment(payment_id):
+    payment = Payment.query.get_or_404(payment_id)
+    payment.status='cancelled'
+    payment.save()
+    return redirect(url_for('get_active_payments'))
+
+
+@app.route('/accept/<payment_id>', methods=['POST'])
+def take_payment(payment_id):
+    accept_payment(payment_id)
+    return redirect(url_for('get_active_payments'))
+
+@app.route('/process_payment/<payment_id>', methods=['POST'])
+def process_payment(payment_id):
+    if request.form['action']=='Cancel Payment':
+        payment = Payment.query.get_or_404(payment_id)
+        payment.status = 'cancelled'
+        payment.save()
+    elif request.form['action']=='Take Payment':
+        accept_payment(payment_id)
+    return redirect(url_for('get_active_payments'))
